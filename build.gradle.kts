82e97fcf0ed0a69a0aefce8d3e8ba494fb28f348
JaneWardSandy <lyj19940214@live.com>
2021-04-16 17:35:59 +0800

    init: initial project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "cn.varsa"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "eclipse-pde-partial"
+

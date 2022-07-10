4e458aca4cc4f002d8ca79b5f0c9aa3d40479f3e
JaneWardSandy <lyj19940214@live.com>
2022-07-10 14:12:10 +0800

    refactor: Rewrite the entire plugin
    BREAKING CHANGE: All code deleted

--- eclipse-pde-partial-idea/src/main/kotlin/cn/varsa/idea/pde/partial/plugin/support/KotlinExt.kt
+++
@@ -1,8 +0,0 @@
-package cn.varsa.idea.pde.partial.plugin.support
-
-import com.intellij.openapi.module.*
-import com.intellij.psi.*
-
-val PsiFile.module: Module? get() = ModuleUtilCore.findModuleForFile(this)
-val PsiClass.module: Module? get() = ModuleUtilCore.findModuleForPsiElement(this)
-val PsiElement.module: Module? get() = ModuleUtilCore.findModuleForPsiElement(this)

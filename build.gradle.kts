731de2115be77bdfea5ffbe986ef30628fc4af6c
JaneWardSandy <lyj19940214@live.com>
2022-07-10 19:31:16 +0800

    build: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,61 @@
 plugins {
-  kotlin("jvm") version "1.6.0"
+  kotlin("jvm") version "1.7.10"
 
-  kotlin("kapt") version "1.6.0" apply false
-  id("org.jetbrains.intellij") version "1.3.1" apply false
+  kotlin("kapt") version "1.7.10" apply false
+  id("org.jetbrains.intellij") version "1.7.0" apply false
 
-  id("org.openjfx.javafxplugin") version "0.0.11" apply false
-  id("org.beryx.jlink") version "2.24.4" apply false
+  id("org.openjfx.javafxplugin") version "0.0.13" apply false
+  id("org.beryx.jlink") version "2.25.0" apply false
 }
 
 group = "cn.varsa"
-version = "1.4.2"
+version = "2.0.0-SNAPSHOT"
 
 repositories {
   mavenLocal()
   mavenCentral()
 }
 
-dependencies {
-  implementation(kotlin("stdlib"))
-}
-
-tasks.register("pack") {
-  group = "partial"
-
-  dependsOn(tasks.getByPath(":eclipse-pde-partial-idea:buildPlugin"))
-  dependsOn(tasks.getByPath(":wishes-launcher:jlinkZip"))
-}
-
 subprojects {
   group = rootProject.group
   version = rootProject.version
 
+  apply {
+    plugin("org.jetbrains.kotlin.jvm")
+  }
+
   repositories {
     mavenLocal()
     mavenCentral()
+  }
+
+  dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+  }
+
+  tasks {
+    compileJava {
+      sourceCompatibility = "11"
+      targetCompatibility = "11"
+    }
+    compileTestJava {
+      sourceCompatibility = "11"
+      targetCompatibility = "11"
+    }
+    compileKotlin {
+      kotlinOptions {
+        jvmTarget = "11"
+        apiVersion = "1.7"
+        languageVersion = "1.7"
+      }
+    }
+    compileTestKotlin {
+      kotlinOptions {
+        jvmTarget = "11"
+        apiVersion = "1.7"
+        languageVersion = "1.7"
+      }
+    }
   }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,4 @@
 rootProject.name = "eclipse-pde-partial"
+
+include(":eclipse-pde-partial-idea")
+project(":eclipse-pde-partial-idea").projectDir = file("idea-plugin")

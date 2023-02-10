b47829e9e0386455ffea49842208fe650e1e2e6c
JaneWardSandy <lyj19940214@live.com>
2023-02-10 11:32:32 +0800

    fix: Only the Kotlin standard library is allowed to use the 'kotlin' package

--- src/main/kotlin/cn/varsa/idea/pde/partial/common/extension/String+Version.kt
+++ src/main/kotlin/cn/varsa/idea/pde/partial/common/extension/String+Version.kt
@@ -1,10 +1,9 @@
 package cn.varsa.idea.pde.partial.common.extension
 
-import cn.varsa.idea.pde.partial.common.version.*
-import kotlin.internal.*
+import cn.varsa.idea.pde.partial.common.version.Version
+import cn.varsa.idea.pde.partial.common.version.VersionRange
 
-@InlineOnly inline fun String?.parseVersion(): Version =
-  if (this != null) Version.parse(unquote()) else Version.emptyVersion
+fun String?.parseVersion(): Version = if (this != null) Version.parse(unquote()) else Version.emptyVersion
 
-@InlineOnly inline fun String?.parseVersionRange(): VersionRange =
+fun String?.parseVersionRange(): VersionRange =
   if (this != null) VersionRange.parse(unquote()) else VersionRange.anyVersionRange
